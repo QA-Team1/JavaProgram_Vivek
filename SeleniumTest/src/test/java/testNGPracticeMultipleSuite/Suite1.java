496757fe3f88c08732cb706152ee3362375de0e6
Vivek22194 <vraparka@amico.com>
2022-11-08 23:10:41 -0500

    Multiple suites execution

---
+++ SeleniumTest/src/test/java/testNGPracticeMultipleSuite/Suite1.java
@@ -0,0 +1,11 @@
+package testNGPracticeMultipleSuite;
+
+import org.testng.annotations.Test;
+
+public class Suite1 {
+
+	@Test
+	public void suite1() {
+		System.out.println("Method of suite 1");
+	}
+}
---
+++ SeleniumTest/src/test/java/testNGPracticeMultipleSuite/Suite2.java
@@ -0,0 +1,11 @@
+package testNGPracticeMultipleSuite;
+
+import org.testng.annotations.Test;
+
+public class Suite2 {
+
+	@Test
+	public void suite2() {
+		System.out.println("Method of suite 2");
+	}
+}
